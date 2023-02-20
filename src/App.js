cd81ee10fb37cdbb9fd7143b8a5b2632e7267a5e
Juljeta Troka <juliet.troka@gmail.com>
2023-02-20 19:19:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email:""
+  })
+
+  function handleChange(event) {
+    let name = event.target.name
+    let value = event.target.value
+    setFormData(
+      {...formData, 
+        [name]:value
+      })
+  }
+
+  function handleSubmit(event){
+    event.preventDefault()
+    console.log(formData)
+    setFormData({firstName: "",lastName: "",email:""
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor='firstName'>First Name</label>
+          <input
+            type="text"
+            placeholder='First Name'
+            name='firstName'
+            onChange={handleChange}
+            value={formData.firstName}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='lastName'>Last Name</label>
+          <input
+            type="text"
+            placeholder='Last Name'
+            name='lastName'
+            onChange={handleChange}
+            value={formData.lastName}
+          />
+        </div>
+
+        <div>
+          <label htmlFor='email'>Email Account</label>
+          <input
+            type="email"
+            placeholder='Email' 
+            name='email'
+            onChange={handleChange}
+            value={formData.email}
+            />
+        </div>
+
+        <div>
+          <button type='submit'>Submit</button>
+        </div>
+
+      </form>
     </div>
   );
 }
